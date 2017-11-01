35674d5dc3c349af9afb9191a83cc678738cb3be
haileebn <hainvyp@gmail.com>
2017-11-02 00:21:37 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var app = express();
+// var server = require("http").createServer(app);
+// server.listen(8080);
+
+app.get("/", function(req, res){
+	// res.send("<font color = red>hello hai nguyen</font>");
+	res.sendFile(__dirname + "/index.html");
+});
+app.get("/infobook", function(req, res){
+	// res.send("<font color = red>hello hai nguyen</font>");
+	res.sendFile(__dirname + "/infobook.html");
+});
+
+
+app.listen(8080, () => {
+	console.log("ser on: 8080");
+});
+
+// app.get("/gioithieu.aspx", function(req, res){
+// 	// res.send("<font color = red>hello hai nguyen</font>");
+// 	res.send("I'M GIOITHIEU.ASPX");
+// });
+
+// app.get("/tinhtong/:so1/:so2", function(req, res){
+// 	// res.send("<font color = red>hello hai nguyen</font>");
+// 	// var n = parseInt(req.params.so1);
+// 	var n = parseInt(req.params.so1);
+// 	var m = parseInt(req.params.so2);
+//  	var sum = m + n;	 
+// 	res.send("Ket qua la: " + sum);
+	
+// });
